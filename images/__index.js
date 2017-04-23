8c302d5cf5a5948321befe8e96fbeff7c463e1bf
John Doe <john.doe@example.com>
2017-04-23 20:49:07 +0300

    Store is updated at 23-Apr-17 20:49:07

---
+++ images/__index.js
@@ -0,0 +1,7 @@
+
+function GitHubFileStoreInfo(options) {
+	Object.assign(this, options, {});
+}
+(function() {
+	window.indexes = {};
+})();window.indexes['b64debd9a5ee28fb9c284b709ef1bbb7'] = new GitHubFileStoreInfo({fileName: 'b64debd9a5ee28fb9c284b709ef1bbb7.JPG'})
