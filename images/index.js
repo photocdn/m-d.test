5fb3065f38c96924fd399217032cb44e3d2b782b
John Doe <john.doe@example.com>
2017-04-23 23:14:17 +0300

    Store is updated at 23-Apr-17 23:14:17

---
+++ images/index.js
@@ -0,0 +1,7 @@
+(function() {
+	window.indexes = {};
+window.indexes['b64debd9a5ee28fb9c284b709ef1bbb7'] = {fileName: 'b64debd9a5ee28fb9c284b709ef1bbb7.JPG'};
+window.indexes['bdc90fc99bafd5d8edb20b40b820821d'] = {fileName: 'bdc90fc99bafd5d8edb20b40b820821d.JPG'};
+window.indexes['e47c30fce12ce06f2f275bea486b22b4'] = {fileName: 'e47c30fce12ce06f2f275bea486b22b4.JPG'};
+window.indexes['__index'] = {fileName: '__index.js'};
+})();
