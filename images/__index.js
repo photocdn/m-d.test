c240b62380a6c08f0c71c91ddf58a6621a347e7f
John Doe <john.doe@example.com>
2017-04-23 21:43:26 +0300

    Store is updated at 23-Apr-17 21:43:26

--- images/__index.js
+++ images/__index.js
@@ -1,3 +1,5 @@
 (function() {
 	window.indexes = {};
+window.indexes['b64debd9a5ee28fb9c284b709ef1bbb7'] = {fileName: 'b64debd9a5ee28fb9c284b709ef1bbb7.JPG'};
+window.indexes['__index'] = {fileName: '__index.js'};
 })();
